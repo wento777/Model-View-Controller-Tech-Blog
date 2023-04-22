8a46bbf25a824e9a5bcb1a34560be30b624576cb
wento <florea_andreiwento@yahoo.com>
2023-04-21 23:42:13 -0500

    added and fixed new cod

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -17,6 +17,7 @@ router.post('/', async (req, res) => {
         ...req.body,
         user_id: req.session.user_id,
       });
+      
   
       res.status(200).json(newPost);
     } catch (err) {
